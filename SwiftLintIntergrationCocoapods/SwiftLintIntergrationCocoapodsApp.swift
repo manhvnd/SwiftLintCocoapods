d731276f0c019769a2e9c6a1fb41f4eed012eede
Manh Vu <vndmanh@tma.com.vn>
2024-08-30 13:47:32 +0700

    Initial Commit

---
+++ SwiftLintIntergrationCocoapods/SwiftLintIntergrationCocoapodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLintIntergrationCocoapodsApp.swift
+//  SwiftLintIntergrationCocoapods
+//
+//  Created by vndmanh on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLintIntergrationCocoapodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
